8e272cb01cb951e3b2b585b338e057fc339bdd65
fh-ivan <ivan.kerin@fluentinhealth.com>
2024-04-15 11:00:40 +0000

    Update

--- workstation.sh
+++ workstation.sh
@@ -3,14 +3,6 @@ if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-if [ ! -f ~/.ssh/github_work ]; then
-    echo "Creating github keys"
-    ssh-keygen -t rsa -N '' -C `git config user.email` -f ~/.ssh/github_work
-    echo -e "\nCopy and paste this at https://github.com/settings/keys\n---------\n"
-    cat ~/.ssh/github_work.pub
-    echo -e "\n\n---------"
-fi
-
 if ! command -v terraform &> /dev/null
 then
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
